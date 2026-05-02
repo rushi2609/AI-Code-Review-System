c4f5511cad6f02d7153ffd5dacbae6704da84a29
rushi <mandadirushivarunreddy@gmail.com>
2026-05-03 02:19:35 +0530

    Completed MERN authentication system

---
+++ backend/controllers/authController.js
@@ -0,0 +1,116 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+
+  try {
+
+    const { name, email, password } = req.body;
+
+    if(!name || !email || !password){
+      return res.status(400).json({
+        success:false,
+        message:"All fields required"
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if(existingUser){
+      return res.status(400).json({
+        success:false,
+        message:"Email already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    const token = jwt.sign(
+      { id:user._id },
+      process.env.JWT_SECRET,
+      { expiresIn:"7d" }
+    );
+
+    res.status(201).json({
+      success:true,
+      token,
+      user:{
+        id:user._id,
+        name:user.name,
+        email:user.email
+      }
+    });
+
+  } catch(error){
+
+    res.status(500).json({
+      success:false,
+      message:error.message
+    });
+
+  }
+
+};
+
+const loginUser = async (req, res) => {
+
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(!user){
+      return res.status(401).json({
+        success:false,
+        message:"Invalid credentials"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+      return res.status(401).json({
+        success:false,
+        message:"Invalid credentials"
+      });
+    }
+
+    const token = jwt.sign(
+      { id:user._id },
+      process.env.JWT_SECRET,
+      { expiresIn:"7d" }
+    );
+
+    res.json({
+      success:true,
+      token,
+      user:{
+        id:user._id,
+        name:user.name,
+        email:user.email
+      }
+    });
+
+  } catch(error){
+
+    res.status(500).json({
+      success:false,
+      message:error.message
+    });
+
+  }
+
+};
+
+module.exports = {
+  registerUser,
+  loginUser
+};
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
